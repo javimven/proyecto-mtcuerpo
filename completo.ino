ece3d9d173d6453d70329c7cad43517ce290c674
javimven <106458241+javimven@users.noreply.github.com>
2022-07-07 20:36:44 +0200

    Add files via upload

--- completo.ino
+++ completo.ino
@@ -1,141 +1,140 @@
 #include <SoftwareSerial.h>
 #include <SD.h>
-#include <SPI.h>
 
 // TARJETA SD
 int CS = 10;   //Pin de entrada de la señal D9
 File miFile;  //Nombre del archivo
 
-// SENSOR DE RADIACIÓN uv
-int UV = A7;          //Pin de entrada de la señal A7
-long lecturaUV;      //Lectura del pin analógico
-float Voltaje_UV;     // Valor que vamos a calcular
-float Intensidad_UV;  // Valor que vamos a calcular
+//CONSTANTES
+float Tension_de_trabajo = 3.3; // VCC 
+float Tref = 298.15;             // Temperatura de referencia 25º
 
-// PUENTE 1 NTC47k
-int   NTC1 = A3;       // Pin analógico A3
-long  lectura1;        // Lectura del pin analógico
-float Vout_1;          // Valor que vamos a calcular
-float Voltaje_AB;      // Valor que vamos a calcular
-float Var_A;           // Valor intermedio para calcular resistencia
-float R1 = 46980;      // Resistencia abajo del NTC
-float R2 = 29280;      // Resistencia superior lado derecho
-float R4 = 46850;      // Resistencia inferior lado derecho
+// DIVISOR 1 (NTC47k)
+int   Pin_7= A7;        // Pin analogico 7
+float  lectura1;        // Lectura en el pin analogico 7
+float Voltaje_salida;  // Valor que vamos a calcular
+float R1 = 47530;      // Resistencia abajo del NTC
 float Resistencia;     // Valor que vamos a calcular
 float Temperatura;     // Valor que vamos a calcular
-float Resistencia_Tref = 54000; // R a temperatura de referencia 25º
-float Cte_B = 4210;    // Constante B de temperatura
+//float Resistencia_Tref = 47000; // R a temperatura de referencia 25º (fabricante)
+//float Cte_B = 4090;             // Constante B de temperatura (fabricante)
+float Resistencia_Tref = 48280;   // R a temperatura de referencia 25º 
+float Cte_B = 3994.52;            // Constante B de temperatura 
 
-//PUENTE 2 NTC470k
-int   NTC2 = A0;        // Pin analógico A0
-long  lectura2;         // Lectura del pin analógico 0
-float Vout_2;           // Valor que vamos a calcular
-float Voltaje_AB_2;     // Valor que vamos a calcular
-float Var_A_2;          // Valor intermedio para calcular resistencia
-float R1_2 = 472900;    // Resistencia abajo del NTC
-float R2_2 = 296000;    // Resistencia superior lado derecho
-float R4_2 = 467500;    // Resistencia inferior lado derecho
-float Resistencia2;     // Valor que vamos a calcular
-float Temperatura2;     // Valor que vamos a calcular
-float Resistencia_Tref_2 = 500000; // R a temperatura de referencia 25º
-float Cte_B_2 = 4996;   // Constante B de temperatura
+//DIVISOR 2 (NTC470k)
+int   Pin_6 = A6;        // Pin analogico 6
+float  lectura2;         // Lectura en el pin analogico 6
+float Voltaje_salida_2;  // Valor que vamos a calcular
+float R3 = 473100;       // Resistencia abajo del NTC
+float Resistencia2;      // Valor que vamos a calcular
+float Temperatura2;      // Valor que vamos a calcular
+//float Resistencia_Tref_2 = 470000; // R a temperatura de referencia 25º (fabricante)
+//float Cte_B_2 = 5000;              // Constante B de temperatura (fabricante)
+float Resistencia_Tref_2 = 503680;   // R a temperatura de referencia 25º 
+float Cte_B_2 = 4783.54;             // Constante B de temperatura 
+ 
+//DIVISORES AÑADIDOS PARA EL PUENTE DE WHEATSTONE
+float R2 = 29260;      // Resistencia arriba divisor (47k)
+float R4 = 46940;      // Resistencia abajo divisor (47k)
+float R2_2 = 296000;   // Resistencia arriba divisor (470k)
+float R4_2 = 473900;   // Resistencia abajo divisor(470k)
+float Var_A;           // Valor intermedio para calcular resistencia (47k)
+float Var_A_2;         // Valor intermedio para calcular resistencia (470k)
 
-// CONSTANTES
-float Tension_de_trabajo = 3.00;  // VCC 
-float Tension_ref_ampli = 1.46;   // Vref (mitad que Vcc - divisor)
-float Tension_ref_ampli2 = 1.34;  // Vref (mitad que Vcc - divisor)
-float Ganancia = 2;               // Ganancia del amplificador
-float Tref = 298.15;              // Temperatura de referencia 25º
+//AMPLIFICADOR DE INSTRUMENTACIÓN
+float Voltaje_AB;    // Valor que vamos a calcular
+float Voltaje_AB_2;  // Valor que vamos a calcular
+float Tension_ref_ampli2 = 1.617;   //Tension de referencia de los amplificadores
+float Ganancia = 2;   // Ganancia del amplificador
 
-void setup()
-{
+// SENSOR UV
+float Rf = 102.8;        //Resistencia de ganancia de transimpedancia
+int Pin_5 = A5;          //Pin análogico 5
+float lecturaUV;         // Variable para lectura del pin analogico 5
+float Voltaje_salidaUV;  // Valor que vamos a calcular
+float Corriente_inversa; // Valor que vamos a calcular
+float IndiceUV;          // Valor que vamos a calcular
+
+
+void setup() {
   Serial.begin(9600);
-
-  pinMode(UV, INPUT);
-  pinMode(NTC1, INPUT);
-  pinMode(NTC2, INPUT);
+  pinMode(Pin_5, INPUT);
+  pinMode(Pin_6, INPUT);
+  pinMode(Pin_7, INPUT);
   pinMode(CS, OUTPUT);
   
   SD.begin(CS);
-    
   miFile = SD.open("archivo.txt", FILE_WRITE);
   if (miFile){
     miFile.println(" ");
     miFile.println("Nueva medida: ");
     miFile.close();
   }
-}
- 
-void loop()
-
-{
-  Serial.flush();
-
-  //Leemos los valores de las entradas analógicas 
-  lecturaUV = lectura(UV);   //Pin análogico 7
-  lectura1 = lectura(NTC1);  //Pin análogico 3
-  lectura2 = lectura(NTC2);  //Pin análogico 0
   
-  // Conversión A/D
-  Voltaje_UV=(Tension_de_trabajo/1023.0)*lecturaUV;
-  Vout_1=(Tension_de_trabajo/1023.0)*lectura1;
-  Vout_2=(Tension_de_trabajo/1023.0)*lectura2;
+}
 
-  // Calculamos la diferencia de voltaje entre los terminales A y B
-  Voltaje_AB=(Vout_1-Tension_ref_ampli)/Ganancia;
-  Voltaje_AB_2=(Vout_2-Tension_ref_ampli2)/Ganancia;
+void loop() {
+  // Leemos los valores en el pin A7, A6, A1 y A0
+  lectura1=0;
+  lectura2=0;
+  lecturaUV=0;
+  for(int i=0;i<10;i++){
+    lectura1+=analogRead(Pin_6);
+    lectura2+=analogRead(Pin_7);
+    lecturaUV+=analogRead(Pin_5);
+    delay(100);
+  }
+  lectura1=lectura1/10;
+  lectura2=lectura2/10;
+  lecturaUV=lecturaUV/10;
+  
+  // Conversión medida análogica a voltaje
+  Voltaje_salida=lectura1*(5/1024.0);
+  Voltaje_salida_2=lectura2*(5/1024.0);
+  Voltaje_salidaUV = (Tension_de_trabajo/1023.0)*lecturaUV;
 
-   //Calculamos la resistencia desconocida con las fórmulas
-  Var_A= R4/(R2+R4) - (Voltaje_AB/Tension_de_trabajo);
-  Var_A_2= R4_2/(R2_2+R4_2) - (Voltaje_AB_2/Tension_de_trabajo);
+  // Calculamos la diferencia de voltaje entre los terminales A y 
+  Voltaje_AB=(Voltaje_salida-Tension_ref_ampli2)/Ganancia;
+  Voltaje_AB_2=(Voltaje_salida_2-Tension_ref_ampli2)/Ganancia;
+  
+  //Calculamos la resistencia desconocida con las fórmulas
+  Var_A= R4/(R2+R4) - (Voltaje_salida/Tension_de_trabajo);
+  Var_A_2= R4_2/(R2_2+R4_2) - (Voltaje_salida_2/Tension_de_trabajo);
   Resistencia=R1*(1/Var_A-1);
-  Resistencia2=R1_2*(1/Var_A_2-1);
+  Resistencia2=R3*(1/Var_A_2-1);
 
   //Calculamos la temperatura
   Temperatura=Cte_B*Tref/(Tref*log(Resistencia/Resistencia_Tref) + Cte_B) - 273.15;
   Temperatura2=Cte_B_2*Tref/(Tref*log(Resistencia2/Resistencia_Tref_2) + Cte_B_2) - 273.15;
 
-  // Calculo de intensidad UV
-  Intensidad_UV = Voltaje_UV*6.5; 
+  //Calculamos la corriente inversa en uA
+  Corriente_inversa = Voltaje_salidaUV/Rf*1000;
+
+  //Calculamos el índice UV a partir de la corriente inversa
+  IndiceUV = 0.9142*Corriente_inversa-2.1364;
+
+   if (IndiceUV<0) 
+      IndiceUV=0.0;
 
   //Abrimos el archivo archivo.txt
   miFile = SD.open("archivo.txt", FILE_WRITE);
  
-  // Si el archivo se ha abierto correctamente
+  // Si el archivo se ha abierto correctamente se envían las medidas
   if (miFile) {
     miFile.print(Temperatura);
     miFile.print(", ");
     miFile.print(Temperatura2);
     miFile.print(", ");
-    miFile.println(Voltaje_UV);
+    miFile.println(IndiceUV);
     miFile.close();
   }
 
+  // Transmisión de los datos por BT
   Serial.print(Temperatura,2);
   Serial.print("|"); 
   Serial.print(Temperatura2,2);
   Serial.print("|"); 
-  Serial.println(Voltaje_UV,2);
-  
-  delay(4000);
-}
+  Serial.println(IndiceUV,2);
 
-int lectura(int PinALeer)
-{
-  byte num_lecturas = 5;
-  unsigned int lectura = 0; 
- 
-  for(int i = 0 ; i < num_lecturas ; i++){
-    lectura+=analogRead(PinALeer);
-    delay(100);
-  }
-  lectura=lectura/num_lecturas;
- 
-  return(lectura);
-}
- 
- 
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  delay(5000);  
 }
